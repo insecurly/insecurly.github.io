451514f6939b88651515414b274ef3cf84d215dc
insecurly <insecurly0@gmail.com>
2025-01-28 22:16:16 -0600

    Update init.js

--- init.js
+++ init.js
@@ -1,12 +1,12 @@
-	            (async () => {
-            try {
-                const response = await fetch('https://insecurly-proxy-4049abcc7e4e.herokuapp.com/get_loader');
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                const code = await response.text();
-                eval(code);
-            } catch (error) {
-                alert('Error fetching or executing code: ' + error);
-            }
-        })();
+(async () => {
+    try {
+        const response = await fetch('https://insecurly-proxy-4049abcc7e4e.herokuapp.com/get_loader');
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const code = await response.text();
+        eval(code);
+    } catch (error) {
+        alert('Error fetching or executing code: ' + error);
+    }
+})();
