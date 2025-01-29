f697b1f4f14ebf0e10c05425a821befc2b0b7b8c
insecurly <insecurly0@gmail.com>
2025-01-29 09:56:53 -0600

    Update init.js

--- init.js
+++ init.js
@@ -1,8 +1,4 @@
 (async () => {
-    var text = document.createElement("h1");
-    text.innerHTML = "Loading.. please wait.";
-
-    document.body.appendChild(text);
     try {
         const response = await fetch('https://insecurly-proxy.fly.dev/get_loader');
         if (!response.ok) {
