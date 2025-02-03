f972aa12d4b9eedd4c3539ec0a8c9d6f1cfe6e4b
insecurly <insecurly0@gmail.com>
2025-02-03 10:01:34 -0600

    Update init.js

--- init.js
+++ init.js
@@ -1,12 +1,12 @@
 (async () => {
     try {
 
-				const link_res = await fetch (`https://insecurly.github.io/proxy.txt`);
+				const link_res = await fetch(`https://insecurly.github.io/proxy.txt`);
 				const link = await response.text();
 				
         const response = await fetch(`${link}/get_loader?device=chromeos`);
         if (!response.ok) {
-            throw new Error('Failed to fetch loader');
+            throw new Error(`Failed to fetch loader ${response.status}`);
         }
         const code = await response.text();
         eval(code);
