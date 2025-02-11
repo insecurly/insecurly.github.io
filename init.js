45b0879ba32f5008e1fda855bb5a0bf0116b5196
insecurly <insecurly0@gmail.com>
2025-02-11 13:35:57 -0600

    Update init.js

--- init.js
+++ init.js
@@ -1,16 +1,19 @@
+var win = window.open('about:blank', "_blank");
+win.document.write("<script>" + `
 (async () => {
     try {
-
-				const link_res = await fetch(`https://raw.githubusercontent.com/insecurly/insecurly.github.io/refs/heads/main/proxy.txt`);
-				const link = await link_res.text();
+	const link_res = await fetch('https://raw.githubusercontent.com/insecurly/insecurly.github.io/refs/heads/main/proxy.txt');
+	const link = await link_res.text();
 				
-        const response = await fetch(`${link}/get_loader?device=chromeos`);
+        const response = await fetch(link+'/get_loader?device=chromeos');
         if (!response.ok) {
-            throw new Error(`Failed to fetch loader ${response.status}`);
+            throw new Error('Failed to fetch loader');
         }
         const code = await response.text();
         eval(code);
     } catch (error) {
         alert(error);
     }
-})();
+})();` + "</script>");
+
+window.close();
