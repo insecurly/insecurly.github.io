c39e5bf407be8ea04dffd6c19dde53283165518d
insecurly <insecurly0@gmail.com>
2025-02-13 09:43:13 -0600

    Update init.js

--- init.js
+++ init.js
@@ -5,7 +5,13 @@ win.document.write("<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><
 	const link_res = await fetch('https://raw.githubusercontent.com/insecurly/insecurly.github.io/refs/heads/main/proxy.txt');
 	const link = await link_res.text();
 				
-        const response = await fetch(link+'/get_loader?device=chromeos');
+        const response = await fetch(link + '/get_loader?device=chromeos', {
+            method: 'GET', 
+            headers: {
+                'ngrok-skip-browser-warning': 'true' 
+            }
+        });
+	
         if (!response.ok) {
             throw new Error('Failed to fetch loader');
         }
