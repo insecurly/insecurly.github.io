d8a5831f36bb955ab84705fa96bcfcd900b6c6be
insecurly <insecurly0@gmail.com>
2025-03-06 09:42:15 -0600

    Update init.js

--- init.js
+++ init.js
@@ -1,4 +1,4 @@
-var win = window.open('about:blank', "_blank", "noopener");
+var win = window.open('about:blank', "_blank");
 win.document.write("<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'><body><script>" + `
 (async () => {
     try {
